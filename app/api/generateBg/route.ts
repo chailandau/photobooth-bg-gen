5743231b2294401a3164e4deecdfb6a433154d59
Chai Landau <clandau@octoml.ai>
2024-06-21 17:02:46 -0400

    upload work

--- app/api/generateBg/route.ts
+++ app/api/generateBg/route.ts
@@ -2,8 +2,8 @@ import fs from 'fs';
 
 import { OctoAIClient } from '@octoai/sdk';
 
-// import * as data from './pastBg.json';
-import * as data from './futureBg.json';
+import * as data from './pastBg.json';
+// import * as data from './futureBg.json';
 
 const octoAi = new OctoAIClient({
   apiKey: process.env.OCTOAI_API_TOKEN,
@@ -12,36 +12,36 @@ const octoAi = new OctoAIClient({
 const negativePrompt =
   'blurred, low resolution, low quality, cartoon, person, people, human';
 
-// const pastPrompt =
-//   ', wide shot, old time, vintage, european, hyperrealistic, high resolution, high quality, realistic, highly detailed.';
+const pastPrompt =
+  'Wide shot, old time, vintage, european, hyperrealistic, high resolution, high quality, realistic, highly detailed. AVOID: cartoon, low resolution, low quality, blurred, person, people, human.';
 
-const futurePrompt =
-  ', wide shot, futuristic, sci-fi, high tech, high resolution, high quality, realistic, highly detailed. The image is hyper-realistic, photographic, high-resolution. AVOID: cartoon, low resolution, low quality, blurred, person, people, human.';
+// const futurePrompt =
+//   'Wide shot, futuristic, sci-fi, high tech, high resolution, high quality, realistic, highly detailed. The image is hyper-realistic, photographic, high-resolution. AVOID: cartoon, low resolution, low quality, blurred, person, people, human.';
 
 export async function POST() {
   try {
     for (let index = 0; index < data.length; index++) {
       const { images } = await octoAi.imageGen.generateSdxl({
-        prompt: data[index].bgDesc + futurePrompt,
+        prompt: data[index].bgDesc + pastPrompt,
         negativePrompt,
-        sampler: 'LCM',
-        checkpoint: 'octoai:lcm_sdxl',
-        steps: 6,
+        checkpoint: 'octoai:RealVisXL',
         numImages: 4,
         cfgScale: 3,
+        steps: 20,
         width: 896,
         height: 1152,
-        stylePreset: 'base',
+        useRefiner: false,
+        stylePreset: 'photographic',
       });
 
       images.forEach((output, i) => {
         const title = data[index].jobTitle.replace(/\s/g, '-').toLowerCase();
-        // const pastFilename = `past-${index}-${title}-result-${i}.jpg`;
-        const futureFilename = `future-${index}-${title}-result-${i}.jpg`;
+        const pastFilename = `past-${title}-result-${i}.jpg`;
+        // const futureFilename = `future-${title}-result-${i}.jpg`;
 
         if (output.imageB64) {
           const buffer = Buffer.from(output.imageB64, 'base64');
-          fs.writeFileSync(futureFilename, buffer);
+          fs.writeFileSync(pastFilename, buffer);
         }
       });
     }
